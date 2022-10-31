cfda0bd221495af2841767eeb24d13322cc4d0fc
RonnyKn <ronnykurniawn@gmail.com>
2022-11-01 02:35:28 +0700

    add case ADD TO CART,REMOVE TO CART

--- src/context/Reducers.js
+++ src/context/Reducers.js
@@ -1,5 +1,12 @@
 export const cartReducer = (state, action) => {
   switch (action.type) {
+    case "ADD TO CART":
+      return { ...state, cart: [...state.cart, { ...action.payload, qty: 1 }] }
+    case "REMOVE FROM CART":
+      return {
+        ...state,
+        cart: state.cart.filter((c) => c.id !== action.payload),
+      }
     default:
       return state
   }
